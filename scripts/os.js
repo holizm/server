3f377b46a22e3afff6f47781668e417b490c84b0
Saeed Nemati <sn.1361@gmail.com>
2025-11-03 07:29:40 -0500

    committed by script

--- scripts/os.js
+++ scripts/os.js
@@ -4,6 +4,14 @@ export const isFile = path => {
     return fs.existsSync(path) && fs.statSync(path).isFile()
 }
 
+export const getContent = path => {
+    if (isFile(path)) {
+        const content = fs.readFileSync(path, "utf8")
+        return content
+    }
+    return ""
+}
+
 export const getDirs = path => {
     return fs.readdirSync('.', { withFileTypes: true })
         .filter(d => d.isDirectory())
---
+++ setup/includeUpdateTerminalTitleConfig.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import { getContent } from '../scripts/os.js'
+
+export default () => {
+    const bashrcPath = '/etc/bash.bashrc'
+    const configPath = '/gesht/server/updateTerminalTitleConfig'
+    const content = getContent(bashrcPath)
+    if (!content.includes(configPath)) {
+        fs.appendFileSync(bashrcPath, `\n. ${configPath}\n`, 'utf8')
+    }
+}
