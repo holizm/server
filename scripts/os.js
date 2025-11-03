d1ef4ace8847bf2a2c1e754842d676ae497126b2
Saeed Nemati <sn.1361@gmail.com>
2025-11-02 23:56:32 -0500

    committed by script

--- scripts/os.js
+++ scripts/os.js
@@ -3,3 +3,10 @@ import fs from 'fs'
 export const isFile = path => {
     return fs.existsSync(path) && fs.statSync(path).isFile()
 }
+
+export const getDirs = path => {
+    return fs.readdirSync('.', { withFileTypes: true })
+        .filter(d => d.isDirectory())
+        .map(d => d.name)
+
+}
