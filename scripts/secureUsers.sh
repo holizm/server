a343c06bdb2a8538e5ca9dad69754db5cf3808eb
Saeed Nemati <sn.1361@gmail.com>
2025-11-03 01:22:25 -0500

    committed by script

--- scripts/secureUsers.sh
+++ scripts/secureUsers.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
 groupadd -f shared
+
+USERS_FILE="/gesth/users"
+
+# Check if file exists
+if [[ ! -f "$USERS_FILE" ]]; then
+    echo "Error: User file '$USERS_FILE' not found!"
+    exit 1
+fi
+
+while IFS= read -r user; do
+    if [[ "$user" =~ ^[[:space:]] || "$user" =~ [[:space:]]$ ]]; then
+        echo "Error: username '$user' has leading or trailing whitespace"
+        continue
+    fi
+    id "$user" >/dev/null 2>&1 || sudo useradd -m -s /bin/bash "$user"
+done < $USERS_FILE
