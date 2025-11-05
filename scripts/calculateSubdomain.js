9c413979042147d48348a2b99cbbbec36e61fe11
Saeed Nemati <sn.1361@gmail.com>
2025-11-05 12:41:00 -0500

    committed by script

---
+++ scripts/calculateSubdomain.js
@@ -0,0 +1,37 @@
+import fs from "fs"
+import path from "path"
+import { info } from "./Logger.js"
+import { getContent, isFile } from "./os.js"
+
+export default params => {
+    const {
+        process
+    } = params
+    const subdomainFile = path.join(process.cwd(), "subdomain")
+    let subdomain = ""
+    if (isFile(subdomainFile)) {
+        subdomain = getContent(subdomainFile).trim()
+    } else if (process.toLowerCase().includes("site") && !process.includes("Api")) {
+        subdomain = process.replace("site", "")
+    } else if (process.endsWith("Api")) {
+        subdomain = process.replace("Api", "")
+        if (subdomain === "site") subdomain = "api"
+        else subdomain = `api.${subdomain}`
+    } else if (process.includes("Panel")) {
+        subdomain = process.replace("Panel", "")
+    } else if (process === "accounts") {
+        subdomain = "accounts"
+    } else if (process === "search") {
+        subdomain = "search"
+    } else if (process === "push") {
+        subdomain = "push"
+    } else if (process === "crawl") {
+        subdomain = "crawl"
+    } else if (process === "databases") {
+        subdomain = "db"
+    } else if (process === "storage") {
+        subdomain = "storage"
+    }
+    info(`Subdomain is ${process.env.Subdomain}`)
+    return subdomain === "" ? "" : `${subdomain.toLowerCase()}.`
+}
