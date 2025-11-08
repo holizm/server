b589dd956daa3e2551ef079dbb0e03017f4efcf7
Saeed Nemati <sn.1361@gmail.com>
2025-11-08 09:46:50 -0500

    committed by script

--- scripts/generateNginxConfig.js
+++ scripts/generateNginxConfig.js
@@ -1,80 +1,11 @@
 import fs from 'fs'
 import path from 'path'
-import { replaceVariables } from './os.js'
+import {
+    getContent,
+    replaceVariables,
+} from './os.js'
 
-const getFileAndParams = params => {
-    const {
-        tenants,
-        process,
-        isDev,
-        home,
-    } = params
-    for (const tenant of tenants) {
-        let tenantName, domain, locales, defaultLocale, roles
-        if (tenant.length === 5) {
-            [tenantName, domain, locales, defaultLocale, roles] = tenant
-            roles = `${roles}`.split(',')
-        } else if (tenant.length === 4) {
-            [tenantName, domain, locales, defaultLocale] = tenant
-            roles = []
-        } else {
-            error(`Incomplete tenant line: ${tenant.join(' ')}`)
-            continue
-        }
-        if (process.endsWith('Panel') || process.endsWith('Api')) {
-            let generateNginx = false
-            if (process === 'siteApi') generateNginx = true
-            else if (role === 'admin' || role === 'site') generateNginx = true
-            else if (roles.length && roles.includes(role)) generateNginx = true
-            if (!generateNginx) continue
-        }
-        params.tenant = tenantName
-        params.domain = domain
-        params.locales = locales
-        params.defaultLocale = defaultLocale
-        for (const cfg of [
-            'httpsRedirect',
-            'certificate',
-            'listen',
-            'proxy'
-        ]) params.file = cfg
-        if (isFile('./hasBasicAuth')) {
-            execSync(
-                `htpasswd -b -c /${instance}/${process}/basicAuth '${params.basicAuthUsername}' '${params.basicAuthPassword}'`,
-                { stdio: 'inherit', shell: '/bin/bash' }
-            )
-            params.file = 'basicAuth'
-        }
-        if (process === 'site' || isFile('./site')) {
-            params.file = 'wwwRedirect'
-            params.file = 'compression'
-            if (isFile('./withCache')) {
-                params.file = 'cacheConfig'
-                params.file = 'cacheUsage'
-                params.file = 'siteWithCache'
-                fs.mkdirSync('cache', { recursive: true })
-                fs.chmodSync('cache', 0o777)
-            } else {
-                params.file = 'site'
-            }
-        } else if (process.endsWith('panel')) {
-            params.file = 'apiAndPanel'
-        } else if (process.endsWith('api')) {
-            params.file = 'apiAndPanel'
-        } else if (process === 'storage') {
-            const result = runOnTerminal(
-                `cat /${instance}/siteApi/compose.yaml | grep :5000 | cut -d ':' -f2 | cut -d '-' -f2`,
-            ).trim()
-            params.randomPort = result
-            params.file = 'storage'
-        } else {
-            params.file = process
-        }
-    }
-}
-
-export default params => {
-    getFileAndParams(params)
+const generate = params => {
     const includes = [
         'certificate',
         'compression',
@@ -108,4 +39,112 @@ export default params => {
     replaceVariables(getContent(sourceFile))
     replaceVariables(getContent(tempFile))
     fs.unlinkSync(tempFile)
+}
+
+export default params => {
+    const {
+        home,
+        isDev,
+        process,
+        role,
+        tenants,
+    } = params
+    for (const tenant of tenants) {
+        let tenantName, domain, locales, defaultLocale, roles
+        if (tenant.length === 5) {
+            [tenantName, domain, locales, defaultLocale, roles] = tenant
+            roles = `${roles}`.split(',')
+        } else if (tenant.length === 4) {
+            [tenantName, domain, locales, defaultLocale] = tenant
+            roles = []
+        } else {
+            error(`Incomplete tenant line: ${tenant.join(' ')}`)
+            continue
+        }
+        if (process.endsWith('Panel') || process.endsWith('Api')) {
+            let generateNginx = false
+            if (process === 'siteApi') generateNginx = true
+            else if (role === 'admin' || role === 'site') generateNginx = true
+            else if (roles.length && roles.includes(role)) generateNginx = true
+            if (!generateNginx) continue
+        }
+        params.tenant = tenantName
+        params.domain = domain
+        params.locales = locales
+        params.defaultLocale = defaultLocale
+        for (const cfg of [
+            'httpsRedirect',
+            'certificate',
+            'listen',
+            'proxy'
+        ]) generate({
+            ...params,
+            file: cfg,
+        })
+        if (isFile('./hasBasicAuth')) {
+            execSync(
+                `htpasswd -b -c /${instance}/${process}/basicAuth '${params.basicAuthUsername}' '${params.basicAuthPassword}'`,
+                { stdio: 'inherit', shell: '/bin/bash' }
+            )
+            generate({
+                ...params,
+                file: 'basicAuth',
+            })
+        }
+        if (process === 'site' || isFile('./site')) {
+            generate({
+                ...params,
+                file: 'wwwRedirect',
+            })
+            generate({
+                ...params,
+                file: 'compression',
+            })
+            if (isFile('./withCache')) {
+                generate({
+                    ...params,
+                    file: 'cacheConfig',
+                })
+                generate({
+                    ...params,
+                    file: 'cacheUsage',
+                })
+                generate({
+                    ...params,
+                    file: 'siteWithCache',
+                })
+                fs.mkdirSync('cache', { recursive: true })
+                fs.chmodSync('cache', 0o777)
+            } else {
+                generate({
+                    ...params,
+                    file: 'site',
+                })
+            }
+        } else if (process.endsWith('panel')) {
+            generate({
+                ...params,
+                file: 'apiAndPanel',
+            })
+        } else if (process.endsWith('api')) {
+            generate({
+                ...params,
+                file: 'apiAndPanel',
+            })
+        } else if (process === 'storage') {
+            const result = runOnTerminal(
+                `cat /${instance}/siteApi/compose.yaml | grep :5000 | cut -d ':' -f2 | cut -d '-' -f2`,
+            ).trim()
+            params.randomPort = result
+            generate({
+                ...params,
+                file: 'storage',
+            })
+        } else {
+            generate({
+                ...params,
+                file: process,
+            })
+        }
+    }
 }
