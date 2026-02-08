3c86c36384d349e5c71ac34be51d68b63b875747
Saeed Nemati <sn.1361@gmail.com>
2026-02-08 05:42:00 -0500

    committed by script

--- setupProd/includeUpdateTerminalTitleConfig.js
+++ setupProd/includeUpdateTerminalTitleConfig.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 import fs from 'fs'
 import { getContent } from '../scripts/os.js'
 
 export default () => {
     const bashrcPath = '/etc/bash.bashrc'
-    const configPath = '/holism/server/updateTerminalTitleConfig'
+    const configPath = '/holism/server/localTerminalUpdateConfig'
     const content = getContent(bashrcPath)
     if (!content.includes(configPath)) {
         fs.appendFileSync(bashrcPath, `\n. ${configPath}\n`, 'utf8')
