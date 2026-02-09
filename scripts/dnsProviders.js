4ed13f4441c47c79a77a748228d4fee06cbf6ddd
Saeed Nemati <sn.1361@gmail.com>
2026-02-09 06:28:51 -0500

    committed by script

---
+++ scripts/dnsProviders.js
@@ -0,0 +1,8 @@
+export default {
+    cloudflare: 'Cloudflare & Google|1.1.1.1 8.8.8.8',
+    shecan: 'shecan.ir|178.22.122.100 185.51.200.2',
+    begzar: 'begzar.ir|185.55.226.26 185.55.225.25',
+    electro: 'electrotm.org|78.157.42.100 78.157.42.101',
+    radar: 'radar.game|10.202.10.10 10.202.10.11',
+    pishgaman: 'pishgaman.net|5.202.100.100 5.202.100.101'
+}
