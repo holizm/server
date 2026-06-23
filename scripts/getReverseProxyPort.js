aa8af2eef6159601589c2f57f15e1954e56adbba
Saeed Nemati <sn.1361@gmail.com>
2026-06-23 17:49:53 +0330

    committed by script

--- scripts/getReverseProxyPort.js
+++ scripts/getReverseProxyPort.js
@@ -10,5 +10,12 @@ export default params => {
         `grep -oP 'proxy_pass\\s+http://localhost:\\K[0-9]+' ${firstConfigFile} | head -n1`,
         true
     ).trim()
-    return reverseProxyPort
+    const runningReverseProxyPort = runOnTerminal(
+        `nginx -T 2>/dev/null | grep -A20 -F "$(grep -m1 server_name ${firstConfigFile} | awk '{print $2}' | tr -d ';')" | grep -oP 'proxy_pass\\s+http://localhost:\\K[0-9]+' | head -n1`,
+        true
+    ).trim()
+    return {
+        reverseProxyPort,
+        runningReverseProxyPort,
+    }
 }
