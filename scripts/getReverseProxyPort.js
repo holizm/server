74af350e7b84e2e54d9bffd0b0a520323b93f686
Saeed Nemati <sn.1361@gmail.com>
2026-06-23 18:53:25 +0330

    committed by script

--- scripts/getReverseProxyPort.js
+++ scripts/getReverseProxyPort.js
@@ -1,3 +1,4 @@
+import { info } from "./logger.js"
 import { runOnTerminal } from './terminal.js'
 
 export default params => {
@@ -6,14 +7,13 @@ export default params => {
         `find ${processPath}/webServer -type f -name '*.config' | head -n1`,
         true
     ).trim()
-    const reverseProxyPort = runOnTerminal(
-        `grep -oP 'proxy_pass\\s+http://localhost:\\K[0-9]+' ${firstConfigFile} | head -n1`,
-        true
-    ).trim()
-    const runningReverseProxyPort = runOnTerminal(
-        `nginx -T 2>/dev/null | grep -A20 -F "$(grep -m1 server_name ${firstConfigFile} | awk '{print $2}' | tr -d ';')" | grep -oP 'proxy_pass\\s+http://localhost:\\K[0-9]+' | head -n1`,
-        true
-    ).trim()
+    info(firstConfigFile)
+    let command = `grep -oP 'proxy_pass\\s+http://localhost:\\K[0-9]+' ${firstConfigFile} | head -n1`
+    info(command)
+    const reverseProxyPort = runOnTerminal(command, true).trim()
+    command = `nginx -T 2>/dev/null | grep -A20 -F "$(grep -m1 server_name ${firstConfigFile} | awk '{print $2}' | tr -d ';')" | grep -oP 'proxy_pass\\s+http://localhost:\\K[0-9]+' | head -n1`
+    info(command)
+    const runningReverseProxyPort = runOnTerminal(command, true).trim()
     return {
         reverseProxyPort,
         runningReverseProxyPort,
