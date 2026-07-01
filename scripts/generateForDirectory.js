8891e5972626835eeb51a753f69ad1b11c3be0e0
Saeed Nemati <sn.1361@gmail.com>
2026-07-01 08:09:28 +0330

    committed by script

--- scripts/generateForDirectory.js
+++ scripts/generateForDirectory.js
@@ -8,6 +8,7 @@ import generateWebServerConfig from './generateWebServerConfig.js'
 import generateCompose from './generateCompose.js'
 import { isFile } from './os.js'
 import pascalize from './pascalize.js'
+import { runOnTerminal } from './terminal.js'
 
 export default params => {
     const {
@@ -17,12 +18,16 @@ export default params => {
     getRandomPort(params)
     params = extract(params)
     const {
-        process,
+        home,
         instance,
+        process,
     } = params
     calculateSubdomain(params)
     params[`${instance}${pascalize(process)}Port`] = params.randomPort
     params.dockerImageName = `ghcr.io/${params.lowercaseOrg}/${params.lowercaseRepo}/${params.lowercaseGitHubImageNameOrProcess}:latest`
     generateCompose(params)
     generateWebServerConfig(params)
+    if (process === 'statics') {
+        runOnTerminal(`rsync -a --delete --exclude='.git' /holism/serverFonts/. ${home}/${instance}/statics/fonts`)
+    }
 }
