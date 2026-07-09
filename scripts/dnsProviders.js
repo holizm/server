952d9386a9d1cb4ea6cfba0d8b2d60f57b5f82b8
Alireza <alireza@hosseinitabar.com>
2026-07-09 15:12:43 +0330

    committed by script

--- scripts/dnsProviders.js
+++ scripts/dnsProviders.js
@@ -4,5 +4,7 @@ export default {
     begzar: 'begzar.ir|185.55.226.26 185.55.225.25',
     electro: 'electrotm.org|78.157.42.100 78.157.42.101',
     radar: 'radar.game|10.202.10.10 10.202.10.11',
-    pishgaman: 'pishgaman.net|5.202.100.100 5.202.100.101'
+    pishgaman: 'pishgaman.net|5.202.100.100 5.202.100.101',
+    arvan: 'arvancloud.ir|217.218.127.127 217.218.155.155',
+    dnspro: 'dnspro.ir|87.107.110.109 87.107.110.110'
 }
