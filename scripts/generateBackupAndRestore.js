61f4056e134f337d743b331a454f0490e1e04f47
Alireza <alireza@hosseinitabar.com>
2026-07-14 11:47:53 +0330

    committed by script

--- scripts/generateBackupAndRestore.js
+++ scripts/generateBackupAndRestore.js
@@ -11,17 +11,13 @@ export default params => {
     } = params
 
     const backupTemplatePath = `${environmentRoot}/server/instance/backup`
-    const backupWithNodeTemplatePath = `${environmentRoot}/server/instance/backupWithNode`
     const restoreTemplatePath = `${environmentRoot}/server/instance/restore`
 
     const backupTarget = `${home}/${instance}/backup`
-    const backupWithNodeTarget = `${home}/${instance}/backupWithNode`
     const restoreTarget = `${home}/${instance}/restore`
 
     replaceVariables(backupTemplatePath, backupTarget, params)
-    replaceVariables(backupWithNodeTemplatePath, backupWithNodeTarget, params)
     replaceVariables(restoreTemplatePath, restoreTarget, params)
     setAsExecutableForTheCurrentUser(backupTarget)
-    setAsExecutableForTheCurrentUser(backupWithNodeTarget)
     setAsExecutableForTheCurrentUser(restoreTarget)
 }
