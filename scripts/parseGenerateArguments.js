0df991e1ba35005f28041fd0e62eb090b01cf7b8
Saeed Nemati <sn.1361@gmail.com>
2026-09-25 11:39:25 +0330

    Use single CSV argument for tenant generation

--- scripts/parseGenerateArguments.js
+++ scripts/parseGenerateArguments.js
@@ -1,27 +1,12 @@
 import { errorAndExit } from './logger.js'
 
 export default args => {
-    const tenantFlagIndex = args.indexOf('--tenants')
-    const tenantCsv = tenantFlagIndex < 0 ? '' : args[tenantFlagIndex + 1]
-    if (tenantFlagIndex >= 0 && !tenantCsv) {
-        errorAndExit('Usage: generate [process] [only] [--tenants tenantOne,tenantTwo]')
+    if (args.length > 1 || args.some(arg => !arg)) {
+        errorAndExit('Usage: generate [tenantOne,tenantTwo]')
     }
-    const positional = tenantFlagIndex < 0
-        ? args
-        : args.filter((arg, index) =>
-            index !== tenantFlagIndex && index !== tenantFlagIndex + 1
-        )
-    if (positional.length > 2 || (positional[1] && positional[1] !== 'only')) {
-        errorAndExit('Usage: generate [process] [only] [--tenants tenantOne,tenantTwo]')
-    }
-    const tenantNames = tenantCsv.split(',').map(name => name.trim()).filter(Boolean)
+    const tenantNames = args.length ? args[0].split(',') : []
     if (tenantNames.some(name => !/^[a-z][A-Za-z0-9]*$/.test(name))) {
         errorAndExit('Tenant names must be camelCase identifiers')
     }
-    const options = {
-        exactMatch: positional[1] === 'only',
-        filterValue: positional[0] || '',
-        tenantNames: [...new Set(tenantNames)],
-    }
-    return options
+    return [...new Set(tenantNames)]
 }
